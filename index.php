a52902adf1f7bfd1ec8291a5fd4f98556fc5d1d7
zansky07 <mhdzans02@gmail.com>
2024-01-05 15:02:30 +0700

    Menambahkan list

--- index.php
+++ index.php
@@ -8,5 +8,10 @@
 <body>
     <h1>Hello World!</h1>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis iusto repellat quae voluptas aut praesentium esse, iure quod fugit veritatis earum atque ipsum, ipsa deserunt modi dolorum a eius qui!</p>
+    <ul>
+        <li>test1</li>
+        <li>test2</li>
+    </ul>
+
 </body>
 </html>
